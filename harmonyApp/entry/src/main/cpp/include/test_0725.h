f39da058473a95177d9425e434a62927f31033eb
hezhengyi <ssyzesb@qq.com>
2025-07-25 16:51:35 +0800

    新增Kotlin调用C中方法的demo
    Signed-off-by: hezhengyi <ssyzesb@qq.com>

---
+++ harmonyApp/entry/src/main/cpp/include/test_0725.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_TEST_0725_H
+#define GLOBAL_TEST_0725_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int testNum(int num);
+
+#ifdef __cplusplus
+};
+#endif
+
+/** @} */
+#endif // GLOBAL_TEST_0725_H
---
+++ harmonyApp/entry/src/main/cpp/test725.cpp
@@ -0,0 +1,6 @@
+
+#include "test_0725.h"
+
+int testNum(int num) {
+    return num + 1;
+}
